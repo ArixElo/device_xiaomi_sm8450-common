9baa80f3fb30cf3e6ff3fa05f9657eb63627036e
Arian <arian.kulmer@web.de>
2024-08-29 18:23:38 +0200

    sm8450-common: Drop embmssl blobs
    Change-Id: I8fadba34ce12ac8371e116404249c7ecd9338e7e

--- extract-files.sh
+++ extract-files.sh
@@ -77,9 +77,6 @@ function blob_fixup() {
         vendor/bin/qcc-trd)
             "${PATCHELF_0_17_2}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
-        vendor/etc/init/init.embmssl_server.rc)
-            sed -i '/interface/d' "${2}"
-            ;;
         vendor/etc/qcril_database/upgrade/config/6.0_config.sql)
             sed -i '/persist.vendor.radio.redir_party_num/ s/true/false/g' "${2}"
             ;;
